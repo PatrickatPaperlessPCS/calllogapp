aefe02a59467a7567863c76539382b8f1a44e5c1
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-10-09 21:15:58 -0500

    added column seperator to csv format in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,7 @@ class ContactsController < ApplicationController
           @contacts = current_user.contacts.order('eventdate DESC').paginate(:page => params[:page], :per_page => 30)
             respond_to do |format|
               format.html
+              format.csv { send_data @contacts.to_csv(col_sep: "\t") }
               format.xls { send_data @contacts.to_csv(col_sep: "\t") }
      end
   end
